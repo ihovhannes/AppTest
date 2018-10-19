822b62a2dbe983774103d2464272a95d8679b02d
Hovhannes Sukiasyan <hovhannessukiasyan@MacBook-Pro-Hovhannes.local>
2018-10-19 12:01:58 +0700

    first commit

---
+++ TestIOSApp/API.swift
@@ -0,0 +1,13 @@
+//
+//  API.swift
+//  TestIOSApp
+//
+//  Created by Hovhannes Sukiasyan on 19.10.2018.
+//  Copyright © 2018 Hovhannes Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class API: NSObject {
+
+}
